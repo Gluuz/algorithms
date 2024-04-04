c5f95f7bb70c2a1dd693fdb23a4bc1c6128d2efc
Gluuz <brunogluszczukjobs@gmail.com>
2024-04-04 08:11:25 -0300

    fix comments

--- grokking-algorithms/chapter-1/test_binary_search.py
+++ grokking-algorithms/chapter-1/test_binary_search.py
@@ -3,12 +3,12 @@ from binary_search import binary_search
 
 
 test_cases = [
-    ([1, 3, 5, 7, 9], 3, 1),  # Elemento está na lista
-    ([1, 3, 5, 7, 9], 9, 4),  # Elemento está na lista
-    ([1, 3, 5, 7, 9], 1, 0),  # Elemento está na lista
-    ([1, 3, 5, 7, 9], 6, None),  # Elemento não está na lista
-    ([], 10, None),  # Lista vazia
-    ([1, 2, 3, 4, 5], 6, None),  # Elemento não está na lista
+    ([1, 3, 5, 7, 9], 3, 1),  # Element is in the list
+    ([1, 3, 5, 7, 9], 9, 4),  # Element is in the list
+    ([1, 3, 5, 7, 9], 1, 0),  # Element is in the list
+    ([1, 3, 5, 7, 9], 6, None),  # Element is not in the list
+    ([], 10, None),  # Empty list
+    ([1, 2, 3, 4, 5], 6, None),  # Element is not in the list
 ]
 
 @pytest.mark.parametrize("lst, item, expected", test_cases)
